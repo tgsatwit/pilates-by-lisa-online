08c00605ef3ebaa59a16813693d42445d1790735
tg <timothygillam@gmail.com>
2025-01-07 12:23:31 +0000

    Automatic commit before Assistant edits

--- components/layout/logo.tsx
+++ components/layout/logo.tsx
@@ -1,11 +1,18 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import LogoImg from '@/public/images/logo-light-lrg.png'
+import Link from "next/link";
+import Image from "next/image";
+import LogoImg from "@/public/images/logo-light-lrg.png";
 
 export default function Logo() {
   return (
     <Link className="inline-flex" href="/" aria-label="Pilates by Lisa">
-      <Image className="max-w-none" src={LogoImg} width={200} height={50} priority alt="Pilates by Lisa" />
+      <Image
+        className="max-w-none"
+        src={LogoImg}
+        width={240}
+        height={60}
+        priority
+        alt="Pilates by Lisa"
+      />
     </Link>
-  )
+  );
 }
